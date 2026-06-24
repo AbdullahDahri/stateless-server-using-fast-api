886498cb1bb789e4282c68b1c6fae92ed232a0fe
Abdullah <abdullahdahri996@gmail.com>
2026-06-24 12:05:48 +0500

    final change is made in first project of stateless server in fastapi. I have added a new endpoint to get user by id. Now you can get user by id using the endpoint /users/{user_id}. If the user is found, it will return the user details, otherwise it will return a message saying "User not found".

--- statelessserver.py
+++ statelessserver.py
@@ -3,8 +3,10 @@ from pydantic import BaseModel
 # pydantic class i used for importing base class which. help us to create the type of data base we want to allowed in post method
 
 app=FastAPI()
+users=[{"id":1,"name":"Abdullah","email":"abdullah@gmail.com","age":30},{"id":2,"name":"Ali","email":"ali@gmail.com","age":25}]
 
 class User(BaseModel):
+    user_id:int
     name:str
     email:str
     age:int
@@ -13,6 +15,16 @@ class User(BaseModel):
 def home():
     return {"message": "API is working!",
             "Status": "Success"}
+
+@app.get("/users/")
+def getusers():
+    return users
+@app.get("/users/{user_id}")
+def getuser(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+    return {"message": "User not found"}
 
 @app.post("/users/")
 def addusers(user: User):
